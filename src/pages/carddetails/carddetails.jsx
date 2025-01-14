2e293d98c9a345b43a4abfba92df321b33a72650
ViniciusDev <viniciusdev10@gmail.com>
2025-01-14 10:57:27 -0300

    finished style home and view detail cards

--- src/pages/carddetails/carddetails.jsx
+++ src/pages/carddetails/carddetails.jsx
@@ -1,92 +1,108 @@
-import{ useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import "./CardDetails.css";
-import Trash from '../../assets/trash.png'
-import Api from '../../services/api'
+import Trash from "../../assets/trash.png";
+import Api from "../../services/api";
+import api from "../../services/api";
 
 function CardDetails() {
-
-    const location = useLocation() // pega o estado passado pelo link
-    const { id, title } = location.state || {} // pegar o titulo do estado ou vazio se cao não tiver
-
-    
+  const location = useLocation(); // pega o estado passado pelo link
+  const { id, title } = location.state || {}; // pegar o titulo do estado ou vazio se cao não tiver
 
   const [accounts, setAccount] = useState([]);
+
+
 
   const handleDelete = (id) => {
     setAccount(accounts.filter((installment) => installment.id !== id));
   };
 
-  async function getAccounts(){
-    try {
-        const response = await Api.get(`/cards/${id}`);
-        
-        const parcels = response.data.accounts || [];
-        setAccount(parcels); // Atualiza o estado com o array de parcelas
-      } catch (error) {
-        console.error("Erro ao buscar detalhes do cartão:", error);
-      }
+  async function deleteAccount(idAccount){
+
+    try{
+
+        await api.delete(`/cards/${id}/${idAccount}`)
+
+    } catch (error){
+        console.error("Erro ao buscar detalhes do cartão ou deletar conta", error);
+    }
+
+    getAccounts()
+
   }
 
+  async function getAccounts() {
+    try {
+      const response = await Api.get(`/cards/${id}`);
 
-  useEffect(() =>{
-    if(id){
-        getAccounts()
+      const parcels = response.data.accounts || [];
+      setAccount(parcels); // Atualiza o estado com o array de parcelas
+    } catch (error) {
+      console.error("Erro ao buscar detalhes do cartão:", error);
     }
-    
-  },[])
+  }
 
+  useEffect(() => {
+    if (id) {
+      getAccounts();
+    }
+  }, []);
+
+  
   return (
     <div className="container">
-    <h1 className="title">{title}</h1>
-  
-    {/* Formulário para adicionar novas contas */}
-    <div className="form">
-      <input className="input" placeholder="Descrição" />
-      <input className="input" placeholder="Valor" />
-      <input className="input" placeholder="Parcel" />
-      <button className="addButton">Adicionar</button>
-    </div>
-  
-    {/* Listagem de contas */}
-    <div className="accounts">
-      {accounts.map((account) => (
-        <div key={account._id} className="account">
-          <h2 className="account-title">Conta</h2>
-          {/*<p>ID da Conta: {account._id}</p>*/}
-  
-          {/* Listagem de parcelas dentro da conta */}
-          <div className="installments">
-            {account.parcels.map((parcel) => (
-              <div key={parcel._id} className="card">
-                <div className="column">
-                  <h3>Parcelas</h3>
-                  <p>{parcel.parcel}</p>
+      <h1 className="title">{title}</h1>
+
+      {/* Formulário para adicionar novas contas */}
+      <div className="form">
+        <input className="input" placeholder="Descrição" />
+        <input className="input" placeholder="Valor" />
+        <input className="input" placeholder="Parcel" />
+        <button className="addButton">Adicionar</button>
+      </div>
+
+      {/* Listagem de contas */}
+      <div className="accounts">
+        {accounts.map((account) => (
+          <div key={account._id} className="account">
+            <h2 className="account-title">Conta</h2>
+            {/*<p>ID da Conta: {account._id}</p>*/}
+
+            {/* Listagem de parcelas dentro da conta */}
+            <div className="buttonDelete">
+              <button
+                className="deleteButton"
+                onClick={() => handleDelete(account._id)}
+              >
+                <img src={Trash} width={20} height={20} alt="Deletar" />
+              </button>
+            </div>
+            <div className="installments">
+              {account.parcels.map((parcel) => (
+                <div key={parcel._id} className="card">
+                  <div className="column">
+                    <h3>Parcelas</h3>
+                    <p>{parcel.parcel}</p>
+                  </div>
+                  <div className="column">
+                    <h3>Valor</h3>
+                    <p>{`R$ ${parcel.parcelAmount}`}</p>
+                  </div>
+                  <div className="column">
+                    <h3>Vencimento</h3>
+                    <p>{parcel.dueDate}</p>
+                  </div>
+                  <div className="column">
+                    <h3>Descrição</h3>
+                    <p>{parcel.description}</p>
+                  </div>
                 </div>
-                <div className="column">
-                  <h3>Vencimento</h3>
-                  <p>{parcel.dueDate}</p>
-                </div>
-                <div className="column">
-                  <h3>Descrição</h3>
-                  <p>{parcel.description}</p>
-                </div>
-                <div className="column">
-                  <button
-                    className="deleteButton"
-                    onClick={() => handleDelete(parcel._id, account._id)}
-                  >
-                    <img src={Trash} width={20} height={20} alt="Deletar" />
-                  </button>
-                </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
-  </div>
-  
   );
 }
 
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,104 +1,118 @@
 import "./style.css";
-import Trash from '../../assets/trash.png'
-import Pincel from '../../assets/pincel.png'
-import api from '../../services/api'
+import Trash from "../../assets/trash.png";
+import Pincel from "../../assets/pincel.png";
+import api from "../../services/api";
 import { useEffect, useState, useRef } from "react";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 function Home() {
+  const [cards, setCards] = useState([]);
 
-  const [cards, setCards] = useState([])
+  const inputTitle = useRef();
+  const inputDate = useRef();
 
-  const inputTitle = useRef()
-  const inputDate = useRef()
+  async function getCards() {
+    const cardsFromApi = await api.get("/cards");
 
-
-  async function getCards(){
-
-      const cardsFromApi = await api.get('/cards')
-
-      setCards(cardsFromApi.data)
-
+    setCards(cardsFromApi.data);
   }
 
   async function createdCard() {
-
-    console.log('Título:', inputTitle.current.value);
-    console.log('Data:', inputDate.current.value);
+    console.log("Título:", inputTitle.current.value);
+    console.log("Data:", inputDate.current.value);
 
     try {
       const title = inputTitle.current.value.trim();
       const date = Number(inputDate.current.value); // Converte para número no frontend
-  
+
       if (!title) {
         alert("O título não pode estar vazio.");
         return;
       }
-  
+
       if (!date || date < 1 || date > 31) {
         alert("A data de vencimento deve ser um número entre 1 e 31.");
         return;
       }
-  
-      const response = await api.post('/cards', { title, date });
-      console.log("Card criado:", response.data.card);
-  
-      getCards(); // Atualiza a lista de cards
-  
-    } catch (error) {
-      console.error("Erro ao criar o card:", error.response?.data?.error || error.message);
-    }
 
+      const response = await api.post("/cards", { title, date });
+      console.log("Card criado:", response.data.card);
+
+      getCards(); // Atualiza a lista de cards
+    } catch (error) {
+      console.error(
+        "Erro ao criar o card:",
+        error.response?.data?.error || error.message
+      );
+    }
   }
 
-  async function deleteCard(id){
+  async function deleteCard(id) {
+    await api.delete(`/cards/${id}`);
 
-    await api.delete(`/cards/${id}`)
-
-    getCards()
-
+    getCards();
   }
 
   useEffect(() => {
-
-    getCards()
-
-  }, [])
-
- 
+    getCards();
+  }, []);
 
   return (
     <>
-      <div className="container">
-        
-          <h1>Cadastre Cartão</h1>
-            <form onSubmit={(e) => e.preventDefault()}>
-              <input type="text" placeholder="Title" className="form-input" ref={inputTitle}/>
-              <input
-                type="number"
-                className="form-input"
-                placeholder="Data de Vencimento" ref={inputDate}
-              />
-              <button type="button" className="form-button" onClick={createdCard}>
-                Cadastrar
-              </button>
-            </form>
+      <div className="containerHome">
+        <h1>Cadastre Cartão</h1>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <input
+            type="text"
+            placeholder="Título"
+            className="form-input"
+            ref={inputTitle}
+          />
+          <input
+            type="number"
+            placeholder="Data de Vencimento"
+            className="form-input"
+            ref={inputDate}
+          />
+          <button type="button" className="form-button" onClick={createdCard}>
+            Cadastrar
+          </button>
+        </form>
 
-          {cards.map(( card ) => (
-
-            <Link to={`/cards/${card._id}`}  key={card._id} className="card-link" state={{ id: card._id , title: card.title}}> 
+        <div className="cards-container">
+          {cards.map((card) => (
+            <Link
+              to={`/cards/${card._id}`}
+              key={card._id}
+              className="card-link"
+              state={{ id: card._id, title: card.title }}
+            >
               <div className="cards">
-                <div>
-                  <p><span>  {card.title} </span></p>
-                  <p><span> {card.date} </span></p>
-                  </div>
-                  <button><img src={Trash} width={20} height={20} onClick={(e) => {e.preventDefault() ; deleteCard(card._id)}} />    </button>
-                  <button><img src={Pincel} width={20} height={20}/>   </button> 
+                <div className="card-info">
+                  <p>
+                    <span>{card.title}</span>
+                  </p>
+                  <p>
+                    <span>{card.date}</span>
+                  </p>
                 </div>
+                <div className="icon-buttons">
+                  <button
+                    onClick={(e) => {
+                      e.preventDefault();
+                      deleteCard(card._id);
+                    }}
+                  >
+                    <img src={Trash} alt="Deletar" width={20} height={20} />
+                  </button>
+                  <button>
+                    <img src={Pincel} alt="Editar" width={20} height={20} />
+                  </button>
+                </div>
+              </div>
             </Link>
-
           ))}
-      
+        </div>
       </div>
     </>
   );
