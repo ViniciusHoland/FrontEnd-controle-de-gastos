9816656181cbd06d9c747a658d2712b4b371b7d5
ViniciusDev <viniciusdev10@gmail.com>
2025-01-15 08:23:27 -0300

    created form register account in frontend e save in database

--- src/pages/carddetails/carddetails.jsx
+++ src/pages/carddetails/carddetails.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect , useRef } from "react";
 import { useLocation } from "react-router-dom";
 import "./CardDetails.css";
 import Trash from "../../assets/trash.png";
@@ -11,18 +11,53 @@ function CardDetails() {
 
   const [accounts, setAccount] = useState([]);
 
-
+  const inputDescription = useRef()
+  const inputAmount  = useRef()
+  const inputParcel  = useRef()
+  const inputCurrentMonth  = useRef()
 
   /*const handleDelete = (id) => {
     setAccount(accounts.filter((installment) => installment.id !== id));
   };*/
 
+  async function createdAccount(){
+
+    try{
+      let amount =  inputAmount.current.value
+      let description =  inputDescription.current.value
+      let parcel = inputParcel.current.value
+      let current = inputCurrentMonth.current.value
+
+      if(current === "marcado"){
+        current = "true"
+      }else {
+        current = "false"
+      }
+
+
+      if(!amount || !description || !parcel){
+        return alert("Preencha todos os campos")
+      }
+
+      const response = await api.post(`/cards/${id}/accounts`, {description,amount,parcel, current})
+      console.log(response.data)
+      getAccounts()
+
+
+
+    } catch(error){
+      console.error("error ao criar conta", error)
+    }
+     
+
+
+
+
+
+  }
+
   async function deleteAccount(idAccount){
     
-    console.log("URL:", `/cards/${id}/accounts/${idAccount}`);
-    console.log("Card ID:", id); // Certifique-se de que o ID do cartão está correto
-    console.log("Account ID:", idAccount); // Certifique-se de que o ID da conta está correto
-
     try{
         
         await api.delete(`/cards/${id}/accounts/${idAccount}`)
@@ -59,10 +94,16 @@ function CardDetails() {
 
       {/* Formulário para adicionar novas contas */}
       <div className="form">
-        <input className="input" placeholder="Descrição" />
-        <input className="input" placeholder="Valor" />
-        <input className="input" placeholder="Parcel" />
-        <button className="addButton">Adicionar</button>
+        <input className="input" placeholder="Descrição" ref={inputDescription} />
+        <input className="input" placeholder="Valor" ref={inputAmount} />
+        <input className="input" placeholder="Parcel" ref={inputParcel}/>
+        <input type="checkbox" value={"marcado"} ref={inputCurrentMonth}/>
+        <button className="addButton" onClick={() => { createdAccount();
+          inputDescription.current.value = "";
+          inputAmount.current.value = "";
+          inputParcel.current.value = "";
+          inputCurrentMonth.current.checked = false; // Desmarca o checkbox
+        }}>Adicionar</button>
       </div>
 
       {/* Listagem de contas */}
