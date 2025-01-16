4beadd538f7bed7302cee6e3a306d593005f132f
ViniciusDev <viniciusdev10@gmail.com>
2025-01-16 09:21:46 -0300

    created fuction calculed all accounts dueDate

--- src/pages/carddetails/carddetails.jsx
+++ src/pages/carddetails/carddetails.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect , useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useLocation } from "react-router-dom";
 import "./CardDetails.css";
 import Trash from "../../assets/trash.png";
@@ -10,64 +10,60 @@ function CardDetails() {
   const { id, title } = location.state || {}; // pegar o titulo do estado ou vazio se cao não tiver
 
   const [accounts, setAccount] = useState([]);
+  const [card, setCard] = useState(null);
+  const [totalNextDue, setTotalNextDue] = useState(0);
 
-  const inputDescription = useRef()
-  const inputAmount  = useRef()
-  const inputParcel  = useRef()
-  const inputCurrentMonth  = useRef()
+  const inputDescription = useRef();
+  const inputAmount = useRef();
+  const inputParcel = useRef();
+  const inputCurrentMonth = useRef();
 
   /*const handleDelete = (id) => {
     setAccount(accounts.filter((installment) => installment.id !== id));
   };*/
 
-  async function createdAccount(){
+  async function createdAccount() {
+    try {
+      let amount = parseFloat(inputAmount.current.value)
+      let description = inputDescription.current.value;
+      let parcel = parseInt(inputParcel.current.value)
+      let currentMonth = inputCurrentMonth.current.value;
 
-    try{
-      let amount =  inputAmount.current.value
-      let description =  inputDescription.current.value
-      let parcel = inputParcel.current.value
-      let current = inputCurrentMonth.current.value
+      
 
-      if(current === "marcado"){
-        current = "true"
-      }else {
-        current = "false"
+      if (currentMonth === "marcado") {
+        currentMonth = "true";
+      } else {
+        currentMonth = "false";
       }
 
+      console.log(currentMonth)
 
-      if(!amount || !description || !parcel){
-        return alert("Preencha todos os campos")
-      }
-
-      const response = await api.post(`/cards/${id}/accounts`, {description,amount,parcel, current})
-      console.log(response.data)
-      getAccounts()
-
-
-
-    } catch(error){
-      console.error("error ao criar conta", error)
+      const response = await api.post(`/cards/${id}/accounts`, {
+        description,
+        amount,
+        parcel,
+        currentMonth,
+      });
+      console.log(response.data);
+      getAccounts();
+      getCard()
+    } catch (error) {
+      console.error("error ao criar conta", error);
     }
-     
-
-
-
-
-
   }
 
-  async function deleteAccount(idAccount){
-    
-    try{
-        
-        await api.delete(`/cards/${id}/accounts/${idAccount}`)
-        getAccounts()
-    } catch (error){
-        console.error("Erro ao buscar detalhes do cartão ou deletar conta", error);
+  async function deleteAccount(idAccount) {
+    try {
+      await api.delete(`/cards/${id}/accounts/${idAccount}`);
+      getAccounts();
+      getCard()
+    } catch (error) {
+      console.error(
+        "Erro ao buscar detalhes do cartão ou deletar conta",
+        error
+      );
     }
-
-   
-
   }
 
   async function getAccounts() {
@@ -76,34 +72,67 @@ function CardDetails() {
 
       const parcels = response.data.accounts || [];
       setAccount(parcels); // Atualiza o estado com o array de parcelas
+      getCard()
     } catch (error) {
       console.error("Erro ao buscar detalhes do cartão:", error);
     }
   }
 
+  async function getCard(){
+
+    try{
+      const response = await api.get(`/cards/${id}`)
+      setCard(response.data)
+      setTotalNextDue(response.data.totalNextDue)
+     
+    }
+    catch (error){
+      console.error("Erro ao buscar os dados do cartão:", error);
+    }
+
+  }
+
+ 
   useEffect(() => {
     if (id) {
       getAccounts();
+      getCard()
     }
   }, []);
 
-
   return (
     <div className="container">
-      <h1 className="title">{title}</h1>
+        <div className="header">
+        <h1 className="title">{title}</h1>
+        <div className="total-next-due">
+          <p>Total da Próxima Fatura:</p>
+          <h2>{`R$ ${totalNextDue.toFixed(2)}`}</h2>
+        </div>
+      </div>
+      <h1 className="title">Contas</h1>
 
       {/* Formulário para adicionar novas contas */}
       <div className="form">
-        <input className="input" placeholder="Descrição" ref={inputDescription} />
+        <input
+          className="input"
+          placeholder="Descrição"
+          ref={inputDescription}
+        />
         <input className="input" placeholder="Valor" ref={inputAmount} />
-        <input className="input" placeholder="Parcel" ref={inputParcel}/>
-        <input type="checkbox" value={"marcado"} ref={inputCurrentMonth}/>
-        <button className="addButton" onClick={() => { createdAccount();
-          inputDescription.current.value = "";
-          inputAmount.current.value = "";
-          inputParcel.current.value = "";
-          inputCurrentMonth.current.checked = false; // Desmarca o checkbox
-        }}>Adicionar</button>
+        <input className="input" placeholder="Parcel" ref={inputParcel} />
+        <input type="checkbox" value={"marcado"} ref={inputCurrentMonth} />
+        <button
+          className="addButton"
+          onClick={() => {
+            createdAccount();
+            inputDescription.current.value = "";
+            inputAmount.current.value = "";
+            inputParcel.current.value = "";
+            inputCurrentMonth.current.checked = false; // Desmarca o checkbox
+          }}
+        >
+          Adicionar
+        </button>
       </div>
 
       {/* Listagem de contas */}
